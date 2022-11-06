e8b918b9721b5497766cf90a2f0201630bc0e9f2
Arthur <artshx@gmail.com>
2022-11-05 23:48:49 -0300

    Enums and traits as arguments.

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,37 +1,56 @@
 
 #[derive(Debug)]
-struct Car {
+enum Brand {
+    Audi,
+    BMW,
+    Hyundai,
+    Ford,
+    UNKNOWN
+}
+
+#[derive(Debug)]
+pub struct Car {
     name: &'static str,
-    price: i64
+    price: f64,
+    brand: Brand
 }
 
 trait Automobile {
-    fn new(name: &'static str) -> Self;
+    fn new(brand: Brand, name: &'static str) -> Self;
     fn get_name(&self) -> &'static str;
     fn print(&self) {
-        println!("I am {}.", self.get_name());
+        println!("I am \"{}\".", self.get_name());
     }
 }
 
 impl Car {
-    // TODO: overload Car::new
-    // fn new(name: &'static str, price: i64) -> Car {
-    //     Car { name: name, price: price }
-    // }
+    fn new_with_price(name: &'static str, price: f64) -> Car {
+        Car { brand: Brand::UNKNOWN, name: name, price: price }
+    }
 }
 
 impl Automobile for Car {
-    fn new(name: &'static str) -> Car {
-        Car { name: name, price: 0 }
+    fn new(brand: Brand, name: &'static str) -> Car {
+        Car { brand: brand, name: name, price: 0. }
     }
     fn get_name(&self) -> &'static str {
         self.name
     }
 }
 
+fn get_name_of_automobile(automobile: &impl Automobile) -> () {
+    let name = automobile.get_name();
+    println!("Name of the automobile: {name}");
+}
+
 fn main() {
     println!("Hello, world!");
     // let p = Point { x:2, y:54 };
-    let car = Car::new("My car");
-    println!("{:?}", car);
+    let car = Car::new(Brand::Ford, "My car");
+    // let other_car = Car::new_with_price("My other car", 5655.45);
+    let car_name = car.get_name();
+    car.print();
+    println!("{:?}; name: {car_name}; brand: {:?}", car, car.brand);
+    // println!("{:?}", other_car);
+    get_name_of_automobile(&car);
 }
