487258413808cb510722bd0ba8c37b839fb0a001
Arthur <artshx@gmail.com>
2022-11-07 23:36:17 -0300

    Watching multiples files, moved watcher factory to its own function.

--- watch_file/src/main.rs
+++ watch_file/src/main.rs
@@ -2,9 +2,44 @@ use std::{env, path::Path};
 use std::thread::sleep;
 use std::time::Duration;
 
-use notify::{Watcher, RecursiveMode};
+use notify::{Watcher, RecursiveMode, RecommendedWatcher};
 use notify::event::EventKind;
 
+// TODO: Do more complex things on callback
+
+fn event_cb(res: Result<notify::Event, notify::Error>) {
+    match res {
+        Ok(event) => {
+            if EventKind::is_modify(&event.kind) {
+                println!("event modified, {:?}", event);
+            } else {
+                println!("Not modified {:?}", event);
+            }
+        },
+        Err(e) => println!("Error: {:?}", e)
+    }
+}
+fn make_watcher(file: &str) -> RecommendedWatcher {
+    // Creating the watcher notifier
+    let mut watcher = notify::recommended_watcher(event_cb).unwrap();
+    let config = notify::Config::default();
+        // .with_compare_contents(false);
+    // config.compare_contents();
+    match watcher.configure(config) {
+        Ok(true) => println!("ok"),
+        Ok(false) => println!("not ok"),
+        Err(e) => println!("Err {:?}", e)
+    }
+
+    
+
+    watch_file(&mut watcher, file);
+    watcher
+}
+
+fn watch_file(watcher: &mut RecommendedWatcher, file: &str) {
+    watcher.watch(Path::new(file), RecursiveMode::NonRecursive);
+}
 
 fn main() {
     println!("Testing watching a file");
@@ -14,24 +49,14 @@ fn main() {
     let file = histfile.unwrap_or(String::from("/home/isportistics/.zsh_history"));
     println!("{}", file);
 
-    // Creating the watcher notifier
-    // TODO: Move to a function, make sure to not drop the watcher outside of the function score
-    let mut watcher = notify::recommended_watcher(|res: Result<notify::Event, notify::Error>| {
-        match res {
-            Ok(event) => {
-                if EventKind::is_modify(&event.kind) {
-                    println!("event modified");
-                }
-            },
-            Err(e) => println!("Error: {:?}", e)
-        }
-    }).unwrap();
-    watcher.watch(Path::new(&file[..]), RecursiveMode::NonRecursive);
+    let mut watcher = make_watcher(&file[..]);
+
+    watch_file(&mut watcher, "/tmp/file");
 
     // Program main loop
-    let mut counter: u8 = 0;
+    let mut counter: u16 = 0;
     loop {
-        if counter > 100 {
+        if counter > 1000 {
             break
         }
         println!("Running ... {counter}");
